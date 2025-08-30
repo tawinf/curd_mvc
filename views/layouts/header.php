71cb9d9e01a585774a11dc0fbd728388711eafa0
tawin@feu <tawin@feu.ac.th>
2025-08-30 18:25:30 +0700

    Create header.php

---
+++ views/layouts/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF--8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ระบบจัดการสินค้า</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>ระบบจัดการสินค้า (CRUD + MVC + PDO)</h1>
+        </div>
+    </header>
+    <main class="container">
