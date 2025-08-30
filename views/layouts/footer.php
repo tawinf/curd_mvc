3546bd3c52a0f60dd397e64890dc79e7e0fd2f8d
tawin@feu <tawin@feu.ac.th>
2025-08-30 18:24:51 +0700

    Create footer.php

---
+++ views/layouts/footer.php
@@ -0,0 +1,9 @@
+</main> <footer>
+        <div class="container">
+            <p>&copy; <?php echo date('Y'); ?> My Awesome Shop. All Rights Reserved.</p>
+        </div>
+    </footer>
+    
+    <script src="js/script.js"></script>
+</body>
+</html>
